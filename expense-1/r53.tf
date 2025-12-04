9002f2509417fbe1278a1ad0a85ae225a95f9f26
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-04 08:35:40 +0530

    terraform

--- expense-1/r53.tf
+++ expense-1/r53.tf
@@ -5,6 +5,6 @@ resource "aws_route53_record" "expense" {
   name    = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
   type    = "A"
   ttl     = 1
-  records = var.instance_names[count.index] == "frontend" ? [aws_instance.terraform[count.index].public_ip] : [aws_instance.terrafom[count.index].private_ip]
+  records = var.instance_names[count.index] == "frontend" ? [aws_instance.terraform[count.index].public_ip] : [aws_instance.terraform[count.index].private_ip]
   allow_overwrite = true
 }

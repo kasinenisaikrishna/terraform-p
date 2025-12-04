ddff24063c2f6d1dc569c7ea5a8869599b1b082d
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-04 17:22:28 +0530

    terraform

--- dynamic/sg.tf
+++ dynamic/sg.tf
@@ -5,10 +5,12 @@ resource "aws_security_group" "allow_ssh_terraform" {
   # terraform will give us a variable with block name
   dynamic "ingress" {
     for_each = var.ingress_rules
-    from_port        = ingress.value["from_port"]
-    to_port          = ingress.value["to_port"]
-    protocol         = ingress.value["protocol"]
-    cidr_blocks      = ingress.value.cidr_blocks  #allow from everyone
+    content {
+        from_port        = ingress.value["from_port"]
+        to_port          = ingress.value["to_port"]
+        protocol         = ingress.value["protocol"]
+        cidr_blocks      = ingress.value.cidr_blocks  #allow from everyone
+    }
   }
 
   #usually we allow everything in egress  

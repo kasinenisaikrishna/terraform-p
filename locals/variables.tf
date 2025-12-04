e52561eb4d45708ca375dba550a4895a6ccd515c
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-04 08:49:14 +0530

    terraform

---
+++ locals/variables.tf
@@ -0,0 +1,21 @@
+variable "instance_names" {
+  type    = list(string)
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+  type = map(any)
+  default = {
+    Project     = "expense"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+# variable "zone_id" {
+#   default = "Z033712422B61CO3J7OXS"
+# }
+
+# variable "domain_name" {
+#   default = "dawsconnect.org"
+# }

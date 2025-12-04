b38bd7ae5ac8dd61c2ee04468a0117f5ec17b77d
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-04 17:19:48 +0530

    terraform

---
+++ dynamic/sg.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+  name        = "allow_sshh" #allow_ssh is already there in my account
+  description = "Allow port number 22 for SSH access"
+
+  # terraform will give us a variable with block name
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    from_port        = ingress.value["from_port"]
+    to_port          = ingress.value["to_port"]
+    protocol         = ingress.value["protocol"]
+    cidr_blocks      = ingress.value.cidr_blocks  #allow from everyone
+  }
+
+  #usually we allow everything in egress  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_sshh"
+  }
+}

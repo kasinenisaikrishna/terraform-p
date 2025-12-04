8ae4446fc842d34c7406a13bb772b20bef1c4b07
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-04 15:31:59 +0530

    terraform

--- remote-state/provider.tf
+++ remote-state/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "81s-remote-state-kasineni"
-    key    = "remote-state-demo"
+    key    = "remote-state-demo" # every repo should have unique key
     region = "us-east-1"
     dynamodb_table = "81s-locking"
   }

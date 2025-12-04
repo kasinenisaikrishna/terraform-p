2569f2b06bc9f7ceb85c98d6810a947292581bb4
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-04 09:03:08 +0530

    terraform

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "allow_ssh_terraform" {
 # if prod create t3.medium, other create t3.micro
 resource "aws_instance" "elasticsearch"{
     ami = data.aws_ami.ami_info.id
-    instance_type = var.environment == "prod" ? "t3.medium" : "t3.micro"
+    instance_type = local.instance_type
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
         Name = "terraform"
--- locals/locals.tf
+++ locals/locals.tf
@@ -1,5 +1,6 @@
 locals {
   domain_name = "dawsconnect.org"
   zone_id = "Z033712422B61CO3J7OXS"
+  instance_type = var.environment == "prod" ? "t3.medium" : "t3.micro"
   # count.index will not work in locals
 }
